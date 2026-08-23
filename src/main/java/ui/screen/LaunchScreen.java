52c14a50633ab6002be36b4f4ac58c67ec92e759
spoon-mouse <spoon@mouse.com>
2026-08-23 14:05:42 +0100

    Txn RBF

--- src/main/java/ui/screen/LaunchScreen.java
+++ src/main/java/ui/screen/LaunchScreen.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 import static ui.input.Input.*;
@@ -23,7 +24,7 @@ public class LaunchScreen {
     private static TextIO textIO = TextIoFactory.getTextIO();
     private static TextTerminal terminal = textIO.getTextTerminal();
 
-    public enum Choice {WALLET, RESTORE, DIGEST, LISTEN, EXIT}
+    public enum Choice {WALLET, RESTORE, DIGEST, LISTEN, DELETE, EXIT}
 
     public LaunchScreen() {
         Context context = Context.getOrCreate();
@@ -35,26 +36,37 @@ public class LaunchScreen {
     }
 
 
-    public static void show(){
-        while(true) {
-            Choice choice = textIO.newEnumInputReader(Choice.class).read(APP_TITLE_LINE);
-            switch (choice) {
-                case WALLET:
-                    load_wallet();
-                    break;
-                case RESTORE:
-                    restore();
-                    break;
-                case DIGEST:
-                    digest();
-                    break;
-                case LISTEN:
-                    listen();
-                    break;
-                case EXIT:
-                    System.exit(0);
+    public static void show() {
+        while (true) {
+            try {
+                Choice choice = textIO.newEnumInputReader(Choice.class).read(APP_TITLE_LINE);
+                switch (choice) {
+                    case WALLET:
+                        load_wallet();
+                        break;
+                    case RESTORE:
+                        restore();
+                        break;
+                    case DIGEST:
+                        digest();
+                        break;
+                    case LISTEN:
+                        listen();
+                        break;
+                    case DELETE:
+                        delete();
+                        break;
+                    case EXIT:
+                        System.exit(0);
+                }
+            } catch (Exception e) {
+                log.error("{} Error ", LaunchScreen.class.getName(), e);
             }
         }
+    }
+
+    private static void delete() throws IOException {
+        Kit.deleteWallet(getWalletName());
     }
 
     private static void listen() {
